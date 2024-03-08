66ad4d48ecaefdd53da45f0665fb2d9c2060b7ef
abbyruth <abbyruthharris@gmail.com>
2024-03-08 14:17:57 -0700

    Mission_09 project about basketball

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>NCAA Basket Teams - March Madness</h1>;
+}
+
+function TeamCard({ school, name, city, state }: Team) {
+  return (
+    <div className="team-card">
+      <h2>{school}</h2>
+      <p>Mascot: {name}</p>
+      <p>
+        Location: {city}, {state}
+      </p>
+    </div>
+  );
+}
+function TeamList() {
+  return (
+    <div className="team-container">
+      {teamsData.teams.map((team: Team, index: number) => (
+        <TeamCard key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
